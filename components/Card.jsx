b574219ef479c5788595f679ffbc97144e281402
VinayKhedkar <arithmetic.chip@gmail.com>
2024-12-23 14:38:17 +0530

    change: styling changes

--- components/Card.jsx
+++ components/Card.jsx
@@ -5,23 +5,25 @@ import styled from 'styled-components';
 import { LazyBlurImage } from './LazyBlurImage';
 import { IoLogoLinkedin } from 'react-icons/io';
 import Link from 'next/link';
+import { FaPhone } from 'react-icons/fa6';
+import { FiMail } from 'react-icons/fi';
 
-const Card = ({ src, link, name, por }) => {
+const Card = ({ image, link, name, por, number, mail }) => {
     return (
         <StyledWrapper>
             <div className="card">
                 <div className="card-inner">
                     <div className="card-front flex flex-col gap-[1rem]">
-                        <div className='w-full h-[250px]'>
+                        <div className="w-full h-[250px]">
                             <LazyBlurImage
-                                src="background.jpg"
+                                src={image}
                                 alt="card image"
                                 rounded={false}
                                 bgColor={false}
-                                className='rounded-lg'
+                                className="rounded-lg"
                             />
                         </div>
-                        <div className="w-full h-auto text-center bg-[#e5dae5] bottom-0">
+                        <div className="w-full h-auto text-center bg-[#FAFAFA] bottom-0">
                             <h3 className="leading-tight-[2rem] text-[2.2rem] font-[600]">
                                 {name}
                             </h3>
@@ -30,9 +32,23 @@ const Card = ({ src, link, name, por }) => {
                     </div>
                     <div className="card-back">
                         <div>
-                            <Link href={link || '#'}>
-                                <IoLogoLinkedin className="md:w-[6.5rem] md:h-[6.5rem] w-[5rem] h-[5rem] cursor-pointer" />
-                            </Link>
+                            <div className="flex flex-col items-center w-full text-center gap-[1rem]">
+                                <h3 className="text-[2.5rem] font-[600]">
+                                    {name}
+                                </h3>
+                                <p className="font-[600] flex items-center gap-[0.5rem]">
+                                    <FaPhone size={18} />
+                                    {'+91' + number}
+                                </p>
+                                <div className="flex justify-center  gap-[1rem]">
+                                    <Link href={link || '#'}>
+                                        <IoLogoLinkedin className="md:w-[4rem] md:h-[4rem] w-[5rem] h-[5rem] cursor-pointer" />
+                                    </Link>
+                                    <Link href={mail || '#'}>
+                                        <FiMail className="md:w-[4rem] md:h-[4rem] w-[5rem] h-[5rem] cursor-pointer" />
+                                    </Link>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -69,11 +85,11 @@ const StyledWrapper = styled.div`
     }
 
     .card-front {
-        background-color: #e5dae5;
+        background-color: #FAFAFA;
         color: #151515;
         display: flex;
         align-items: center;
-        border: 10px solid #e5dae5;
+        border: 10px solid #FAFAFA;
         border-radius: 10px;
         justify-content: center;
         font-size: 24px;
@@ -81,11 +97,11 @@ const StyledWrapper = styled.div`
     }
 
     .card-back {
-        background-color: #880088;
+        background-color: #D5C9A8;
         color: #fff;
         display: flex;
         align-items: center;
-        border: 10px solid #880088;
+        border: 2px solid #000000;
         border-radius: 10px;
         justify-content: center;
         font-size: 24px;
