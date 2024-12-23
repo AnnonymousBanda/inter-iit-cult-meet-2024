2b9025f5673f8227573bbad0bc9c948f064d46bf
Ankit Bhagat <annonymous007banda@gmail.com>
2024-12-23 09:34:33 +0530

    fix: fix the layout and style of footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -12,6 +12,7 @@ import {
     FaInstagram,
     FaYoutube,
 } from 'react-icons/fa';
+import { LazyBlurImage } from './LazyBlurImage';
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -26,153 +27,69 @@ const FooterContent = () => {
                     alt="admin block"
                 />
                 <h2 className="text-[#d7d3c8] uppercase xl:text-[3.6rem] md:text-[2.7rem] text-[1.5rem]">
-                    Indian Institute of Technology, Patna
+                    Inter IIT Cultural Meet 2024
                 </h2>
             </div>
+            <div className="flex flex-row items-center gap-[3rem] flex-wrap justify-center content-center">
+                <Link href="/" className="w-fit">
+                    <h3 className="text-[#d7d3c8] text-center w-fit">Home</h3>
+                </Link>
+                <Link
+                    href="https://iitp.ac.in/gymkhana"
+                    target="_blank"
+                    className="w-fit"
+                >
+                    <h3 className="text-[#d7d3c8] text-center w-fit">
+                        Gymkhana
+                    </h3>
+                </Link>
+                <Link href="/leaderboard" className="w-fit">
+                    <h3 className="text-[#d7d3c8] text-center w-fit">
+                        Leaderboard
+                    </h3>
+                </Link>
+                <Link href="/contact-us" className="w-fit">
+                    <h3 className="text-[#d7d3c8] text-center w-fit">
+                        Contact&nbsp;Us
+                    </h3>
+                </Link>
+            </div>
 
-            <div className="flex lg:gap-[7rem] py-[2rem] gap-[5rem] flex-wrap justify-center">
-                <div className="flex flex-col gap-[2rem] items-center">
-                    <h2 className="xl:text-[3.6rem] md:text-[2.7rem] text-[1.8rem]">
-                        Useful&nbsp;Links
-                    </h2>
-                    <div className="flex flex-col items-center gap-[1rem] ">
-                        <Link href="https://academics.iitp.ac.in/">
-                            <p className="text-[#d7d3c8] lg:text-[2.5rem]">
-                                Academics
-                            </p>
-                        </Link>
-                        <Link href="/admissions">
-                            <p className="text-[#d7d3c8]  lg:text-[2.5rem]">
-                                Admissions
-                            </p>
-                        </Link>
-                        <Link href="/about">
-                            <p className="text-[#d7d3c8]  lg:text-[2.5rem]">
-                                About Us
-                            </p>
-                        </Link>
-                        <Link href="/visit">
-                            <p className="text-[#d7d3c8]  lg:text-[2.5rem]">
-                                Visit&nbsp;IITP
-                            </p>
-                        </Link>
+            <div className="flex lg:gap-[7rem] py-[2rem] gap-[5rem] w-full flex-wrap justify-center items-center">
+                <div className="flex flex-row justify-around gap-[4rem] items-center">
+                    <div className="md:w-[100px] md:h-[100px] w-[75px] h-[75px]">
+                        <LazyBlurImage
+                            src="inter-iit.png"
+                            alt="inter iit cult logo"
+                        />
                     </div>
-                </div>
-
-                <div className="flex flex-col gap-[2rem] items-center">
-                    <h2 className="xl:text-[3.6rem] md:text-[2.7rem] text-[1.8rem]">
-                        Useful&nbsp;Links
-                    </h2>
-                    <div className="flex flex-col items-center gap-[1rem] ">
-                        <Link href="https://academics.iitp.ac.in/">
-                            <p className="text-[#d7d3c8] lg:text-[2.5rem]">
-                                Academics
-                            </p>
-                        </Link>
-                        <Link href="/admissions">
-                            <p className="text-[#d7d3c8]  lg:text-[2.5rem]">
-                                Admissions
-                            </p>
-                        </Link>
-                        <Link href="/about">
-                            <p className="text-[#d7d3c8]  lg:text-[2.5rem]">
-                                About Us
-                            </p>
-                        </Link>
-                        <Link href="/visit">
-                            <p className="text-[#d7d3c8]  lg:text-[2.5rem]">
-                                Visit&nbsp;IITP
-                            </p>
-                        </Link>
-                    </div>
-                </div>
-                <div className="flex flex-col gap-[2rem] items-center">
-                    <h2 className="xl:text-[3.6rem] md:text-[2.7rem] text-[1.8rem]">
-                        Useful&nbsp;Links
-                    </h2>
-                    <div className="flex flex-col items-center gap-[1rem] ">
-                        <Link href="https://academics.iitp.ac.in/">
-                            <p className="text-[#d7d3c8] lg:text-[2.5rem]">
-                                Academics
-                            </p>
-                        </Link>
-                        <Link href="/admissions">
-                            <p className="text-[#d7d3c8]  lg:text-[2.5rem]">
-                                Admissions
-                            </p>
-                        </Link>
-                        <Link href="/about">
-                            <p className="text-[#d7d3c8]  lg:text-[2.5rem]">
-                                About Us
-                            </p>
-                        </Link>
-                        <Link href="/visit">
-                            <p className="text-[#d7d3c8]  lg:text-[2.5rem]">
-                                Visit&nbsp;IITP
-                            </p>
-                        </Link>
-                    </div>
-                </div>
-                <div className="flex flex-col gap-[2rem] items-center">
-                    <h2 className="xl:text-[3.6rem] md:text-[2.7rem] text-[1.8rem]">
-                        Useful&nbsp;Links
-                    </h2>
-                    <div className="flex flex-col items-center gap-[1rem] ">
-                        <Link href="https://academics.iitp.ac.in/">
-                            <p className="text-[#d7d3c8] lg:text-[2.5rem]">
-                                Academics
-                            </p>
-                        </Link>
-                        <Link href="/admissions">
-                            <p className="text-[#d7d3c8]  lg:text-[2.5rem]">
-                                Admissions
-                            </p>
-                        </Link>
-                        <Link href="/about">
-                            <p className="text-[#d7d3c8]  lg:text-[2.5rem]">
-                                About Us
-                            </p>
-                        </Link>
-                        <Link href="/visit">
-                            <p className="text-[#d7d3c8]  lg:text-[2.5rem]">
-                                Visit&nbsp;IITP
-                            </p>
-                        </Link>
+                    <div className="md:w-[200px] md:h-[100px] w-[150px] h-[75px]">
+                        <LazyBlurImage
+                            src="gymkhana.gif"
+                            alt="gymkhana logo"
+                            rounded={false}
+                        />
                     </div>
                 </div>
             </div>
 
-            <div className="flex md:gap-[10rem] gap-[1.5rem] text-[2rem] md:justify-start justify-center">
+            <div className="flex md:gap-[10rem] gap-[3rem] text-[2rem] md:justify-start justify-center">
                 <Link
-                    href="https://www.facebook.com/IITPatna"
+                    href="https://www.facebook.com/gymkhana.iitp/"
                     target="_blank"
                     aria-label="Facebook"
                 >
                     <FaFacebookF className="text-[20px] sm:text-[25px] md:text-[30px]" />
                 </Link>
                 <Link
-                    href="https://twitter.com/IITPatna"
-                    target="_blank"
-                    aria-label="Twitter"
-                >
-                    <FaTwitter className="text-[20px] sm:text-[25px] md:text-[30px]" />
-                </Link>
-                <Link
-                    href="https://www.linkedin.com/school/iitpatna"
-                    target="_blank"
-                    aria-label="LinkedIn"
-                >
-                    <FaLinkedinIn className="text-[20px] sm:text-[25px] md:text-[30px]" />
-                </Link>
-                <Link
-                    href="https://www.instagram.com/IITPatna"
+                    href="https://www.instagram.com/iitpatna_gymkhana/"
                     target="_blank"
                     aria-label="Instagram"
                 >
                     <FaInstagram className="text-[20px] sm:text-[25px] md:text-[30px]" />
                 </Link>
                 <Link
-                    href="https://www.youtube.com/IITPatna"
+                    href="https://www.youtube.com/@studentgymkhanaiitpatna"
                     target="_blank"
                     aria-label="YouTube"
                 >
@@ -182,7 +99,7 @@ const FooterContent = () => {
             <div className="flex justify-between w-full mt-4">
                 <div>
                     <p className="lg:text-[1.8rem] text-[1.2rem]">
-                        &copy;Indian Institute of Technology, Patna
+                        &copy; Gymkhana, IIT Patna
                     </p>
                 </div>
                 <div>
@@ -245,7 +162,7 @@ const Footer = () => {
         <footer className="footer bg-transparent bg-[#141516] h-auto text-white text-[1.6rem] relative w-full font-home scroll-smooth">
             <div className="w-full h-auto flex flex-col justify-between items-center bg-transparent">
                 <svg
-                    className="w-full h-screen absolute top-[0] z-[100] bg-transparent pointer-events-none"
+                    className="w-full h-screen absolute top-[0] z-[10] bg-transparent pointer-events-none"
                     viewBox="0 0 500 250"
                     preserveAspectRatio="none"
                     fill="#f0f0f0"
